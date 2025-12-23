7c6d4b0343176469f951d18b15d1a1e798fc350c
JoseRodriquez <jose.rodriquez@devitech.com.co>
2025-12-23 09:37:53 -0500

    feat(reports): Cambio en funcion para obtener ReportesMedios

--- context/reports/domain/value_object/constants/constants.go
+++ context/reports/domain/value_object/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	QUERY_GET_DAY_CLOSING_REPORT = "select * from reporteria_cierres.fnc_consultar_cierre_dia(i_fecha => $1);"
+	QUERY_GET_DAY_CLOSING_REPORT = "select reporteria_cierres.fnc_consultar_cierre_dia($1);"
 	QUERY_GET_FUEL_REPORT        = `
 SELECT
     COALESCE(SUM(((datos->>'lt_data')::json ->> 'TotalVentasSistema')::numeric), 0)        AS total_ventas_combustible,
